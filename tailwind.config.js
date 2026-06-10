99fcb8890bc08a6d1d875d3f39ea214fdea5361e
brettcb5 <brettcb2026@gmail.com>
2026-06-10 09:02:15 -0400

    Fix: Add local source files to Tailwind CSS content configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ import config from '@anythingai/app/tailwind.config'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
+		'./src/**/*.{js,ts,jsx,tsx}',
+		'./src/app/**/*.{js,ts,jsx,tsx}',
 		'./node_modules/@anythingai/app/**/*.{js,ts,jsx,tsx}'
 	],
 	...config
